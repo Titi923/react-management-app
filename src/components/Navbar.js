8b65d9362a86fdf82e0eb3589df01f76b15a94ec
Titi923 <petrisor.buciuta92@gmail.com>
2023-01-01 16:57:57 +0200

    Changing the styles a little to look better

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -9,8 +9,10 @@ export default function Navbar() {
     <nav className='navbar'>
       <ul>
         <li className="logo">
-          <img src={Logo} alt="Logo" />
-          <span className='logo-title'>ManagementApp</span>
+          <Link to="/">
+            <img src={Logo} alt="Logo" />
+            <span className='logo-title'>ManagementApp</span>
+          </Link>
         </li>
 
         <li><Link to="/login">Login</Link></li>
