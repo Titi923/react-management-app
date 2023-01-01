eb6bb666f2379bc985e90d09edf8372beb79dc60
Titi923 <petrisor.buciuta92@gmail.com>
2023-01-01 18:21:45 +0200

    Create the Signup Page

--- src/pages/signup/Signup.js
+++ src/pages/signup/Signup.js
@@ -1,12 +1,64 @@
+import { useState } from 'react';
 
-
-// styles
-import './Signup.css'
+// styles & icons
+import './Signup.css';
+import uploadIcon from '../../assets/upload_icon.svg';
 
 export default function Signup() {
+  const [displayName, setDisplayName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [thumbnail, setThumbnail] = useState(null);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    console.log(displayName, email, password);
+    
+    setDisplayName('')
+    setEmail('')
+    setPassword('')
+    setThumbnail(null)
+  };
+
   return (
-    <div>
-      Signup
-    </div>
-  )
+    <form className="auth-form" onSubmit={handleSubmit}>
+      <h2 className="text-center">Sign up</h2>
+      <label>
+        <span>Display Name:</span>
+        <input
+          required
+          type="displayName"
+          onChange={(e) => setDisplayName(e.target.value)}
+          value={displayName}
+        />
+      </label>
+      <label>
+        <span>Email:</span>
+        <input
+          required
+          type="email"
+          onChange={(e) => setEmail(e.target.value)}
+          value={email}
+        />
+      </label>
+      <label>
+        <span>Password:</span>
+        <input
+          required
+          type="password"
+          onChange={(e) => setPassword(e.target.value)}
+          value={password}
+        />
+      </label>
+      <label>
+        <span>Upload Thumbnail:</span>
+        <label class="custom-file-upload">
+          <img src={uploadIcon} alt="upload icon" />Choose Photo
+          <input  type="file" />
+        </label>
+      </label>
+      <button className="btn">Signup</button>
+    </form>
+  );
 }
